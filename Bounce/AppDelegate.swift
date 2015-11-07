04e72a41785011a8db978eb042e9ae474ea86d89
Marlow Charite <marlowcharite@gmail.com>
2015-11-07 15:36:46 -0500

    Initial Commit

---
+++ Bounce/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bounce
+//
+//  Created by Marlow Charite on 11/7/15.
+//  Copyright © 2015 Marlow Charite. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Bounce/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bounce
+//
+//  Created by Marlow Charite on 11/7/15.
+//  Copyright © 2015 Marlow Charite. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

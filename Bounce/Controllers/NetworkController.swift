0276939a35eb8258f9707e5aaf81cb541251b989
Marlow Charite <marlowcharite@gmail.com>
2015-11-07 19:10:14 -0500

    add access tokens to project

---
+++ Bounce/Controllers/NetworkController.swift
@@ -0,0 +1,14 @@
+//
+// Created by Marlow Charite on 11/7/15.
+// Copyright (c) 2015 Marlow Charite. All rights reserved.
+//
+
+import Foundation
+
+class NetworkController {
+
+    enum DribbleAccessToken {
+        case ClientID("")
+    }
+
+}

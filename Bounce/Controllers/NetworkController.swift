465b82fa9eb152eae463d62faf59c2164dbb8beb
Marlow Charite <marlowcharite@gmail.com>
2015-11-07 19:10:27 -0500

    add token values

--- Bounce/Controllers/NetworkController.swift
+++ Bounce/Controllers/NetworkController.swift
@@ -7,8 +7,10 @@ import Foundation
 
 class NetworkController {
 
-    enum DribbleAccessToken {
-        case ClientID("")
+    enum DribbbleAccessToken: String {
+        case ClientID = "50ea8c3e8c0a2637c41101e9591bb65f8c0b36a600db9a34d1a3a53a83ab7f36"
+        case ClientSecret = "d59ef819b1b7eec961be47e540bbb25cc792cc622c785bbf2780e0b3bd7659e1"
+        case ClientAccessToken = "2517564562baea3d8df083ef5c60b0f4ca8d6a802857ff82329b112808977b77"
     }
 
 }

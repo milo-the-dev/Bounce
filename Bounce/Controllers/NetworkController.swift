eab790e04c54301cec1e5ed3cc6c1d0e8e02c406
Marlow Charite <marlowcharite@gmail.com>
2015-11-07 21:11:42 -0500

    pull DribbbleAccessToken enum out of NetworkController class

--- Bounce/Controllers/NetworkController.swift
+++ Bounce/Controllers/NetworkController.swift
@@ -7,15 +7,15 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-class NetworkController {
+private enum DribbbleAccessToken: String {
+    case ClientID = "50ea8c3e8c0a2637c41101e9591bb65f8c0b36a600db9a34d1a3a53a83ab7f36"
+    case ClientSecret = "d59ef819b1b7eec961be47e540bbb25cc792cc622c785bbf2780e0b3bd7659e1"
+    case ClientAccessToken = "2517564562baea3d8df083ef5c60b0f4ca8d6a802857ff82329b112808977b77"
+}
 
-    enum DribbbleAccessToken: String {
-        case ClientID = "50ea8c3e8c0a2637c41101e9591bb65f8c0b36a600db9a34d1a3a53a83ab7f36"
-        case ClientSecret = "d59ef819b1b7eec961be47e540bbb25cc792cc622c785bbf2780e0b3bd7659e1"
-        case ClientAccessToken = "2517564562baea3d8df083ef5c60b0f4ca8d6a802857ff82329b112808977b77"
-    }
+class NetworkController {
     
-    static let hostUrl = NSURL(string: "https://api.dribbble.com/v1/")
+    static private let hostUrl = NSURL(string: "https://api.dribbble.com/v1/")
     private let session: NSURLSession
     
     init(session: NSURLSession = NSURLSession.sharedSession()) {

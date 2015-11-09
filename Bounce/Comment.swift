297fa34a799c6d01806ccd1fdb51f7bdcbbd45b6
Jetrii <Jetrii@gmail.com>
2015-11-08 21:23:23 -0500

    fix dates in Comment class

--- Bounce/Comment.swift
+++ Bounce/Comment.swift
@@ -5,11 +5,11 @@ class Comment {
     private (set) var body: String
     private (set) var likesCount: Int
     private (set) var likesUrl: String
-    private (set) var createdAt: String
-    private (set) var updatedAt: String
+    private (set) var createdAt: NSDate
+    private (set) var updatedAt: NSDate
     private (set) var user: User
 
-    init(id: Int, body: String, likesCount: Int = 0, likesUrl: String, createdAt: String, updatedAt: String, user: User) {
+    init(id: Int, body: String, likesCount: Int = 0, likesUrl: String, createdAt: NSDate, updatedAt: NSDate, user: User) {
         self.id = id
         self.body = body
         self.likesCount = likesCount
